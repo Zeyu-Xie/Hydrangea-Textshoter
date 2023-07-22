c3bc5b5a4f16593f4ffe6b993525d83b1d29db5d
Acan <acanthuridae@foxmail.com>
2023-07-22 23:48:58 +0800

    .

--- page/src/App.jsx
+++ page/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import axios from "axios"
+import "./App.css"
+import hydrangea from "./hydrangea.png"
 
 class App extends React.Component {
 
@@ -19,43 +21,55 @@ class App extends React.Component {
         return (
             <div className="container">
 
-                <div class="input-group mb-3">
-                    <span class="input-group-text">Source Text</span>
-                    <textarea className="form-control" aria-label="Source Text" ref={this.text} defaultValue={"# Please input your text here."}></textarea>
+                <div className="row">
+
+                    <div className="d-flex col-4 align-content-center" style={{height: "100vh"}}>
+                        <img src={hydrangea} alt="" style={{ margin: "auto", border: "solid rgba(0,0,0,0.2) 1px" }} className="col-12" />
+                    </div>
+                    <div className="col-8">
+                        <div class="input-group mb-3">
+                            <span class="input-group-text">Source Text</span>
+                            <textarea className="form-control" aria-label="Source Text" ref={this.text} defaultValue={"# Please input your text here."}></textarea>
+                        </div>
+
+                        <div className="input-group mb-3">
+                            <span class="input-group-text">Margin X</span>
+                            <input type="text" className="form-control" placeholder="marginX" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.marginX} defaultValue={10} />
+                        </div>
+
+                        <div className="input-group mb-3">
+                            <span class="input-group-text">Margin Y</span>
+                            <input type="text" className="form-control" placeholder="marginY" aria-describedby="button-addon2" ref={this.marginY} defaultValue={10} />
+                        </div>
+
+                        <div className="input-group mb-3">
+                            <span class="input-group-text">Font Size</span>
+                            <input type="text" className="form-control" placeholder="fontSize" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.fontSize} defaultValue={16} />
+                        </div>
+
+                        <div className="input-group mb-3">
+                            <span class="input-group-text">Line Space</span>
+                            <input type="text" className="form-control" placeholder="lineSpace" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.lineSpace} defaultValue={4} />
+                        </div>
+
+                        <div className="input-group mb-3">
+                            <span class="input-group-text">Width</span>
+                            <input type="text" className="form-control" placeholder="width" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.width} defaultValue={576} />
+                        </div>
+
+                        <div className="input-group mb-3">
+                            <span class="input-group-text">Color</span>
+                            <input type="text" className="form-control" placeholder="color" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.color} defaultValue={"black"} />
+                            <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={this.submit} >Button</button>
+                        </div>
+                    </div>
+
                 </div>
 
-                <div className="input-group mb-3">
-                    <span class="input-group-text">Margin X</span>
-                    <input type="text" className="form-control" placeholder="marginX" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.marginX} defaultValue={10} />
-                </div>
 
-                <div className="input-group mb-3">
-                    <span class="input-group-text">Margin Y</span>
-                    <input type="text" className="form-control" placeholder="marginY" aria-describedby="button-addon2" ref={this.marginY} defaultValue={10} />
-                </div>
-
-                <div className="input-group mb-3">
-                    <span class="input-group-text">Font Size</span>
-                    <input type="text" className="form-control" placeholder="fontSize" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.fontSize} defaultValue={16} />
-                </div>
-
-                <div className="input-group mb-3">
-                    <span class="input-group-text">Line Space</span>
-                    <input type="text" className="form-control" placeholder="lineSpace" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.lineSpace} defaultValue={4} />
-                </div>
-
-                <div className="input-group mb-3">
-                    <span class="input-group-text">Width</span>
-                    <input type="text" className="form-control" placeholder="width" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.width} defaultValue={576} />
-                </div>
-
-                <div className="input-group mb-3">
-                    <span class="input-group-text">Color</span>
-                    <input type="text" className="form-control" placeholder="color" aria-label="Recipient's username" aria-describedby="button-addon2" ref={this.color} defaultValue={"black"} />
-                    <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={this.submit} >Button</button>
-                </div>
 
                 <a href="http://127.0.0.1:15371/textshoter/api/downloadImage">Download</a>
+
 
             </div>
         );
