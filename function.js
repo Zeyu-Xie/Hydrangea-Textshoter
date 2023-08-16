3b065099410baeab10d3c747f23b12eca319f29b
Acan <xie.zeyu20@gmail.com>
2023-08-16 14:45:14 +0800

    .

--- function.js
+++ function.js
@@ -1,5 +1,23 @@
 
-html2canvas(document.getElementById("input")).then(canvas=>{
-    const url = canvas.toDataURL("image/png")
-    console.log(url)
-})
+
+
+
+const input=document.getElementById("input")
+const submit = document.getElementById("submit")
+const middle = document.getElementById("middle")
+const result = document.getElementById("result")
+
+submit.onclick = () => {
+
+    middle.innerText=input.value
+    result.style.display="none"
+    middle.style.display="block"
+    html2canvas(middle).then(canvas => {
+        result.src = canvas.toDataURL("image/png")
+    }).then(()=>{
+        middle.style.display="none"
+        result.style.display="block"
+    }).catch(err=>{
+        console.log("ERROR",err)
+    })
+}
