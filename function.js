75f38b47bd4a45c06d8207e98677a2b28b42fad7
Acan <xie.zeyu20@gmail.com>
2023-08-16 12:21:36 +0800

    .

--- function.js
+++ function.js
@@ -1,20 +1,5 @@
 
-// const input=document.getElementById("input")
-// const submit=document.getElementById("submit")
-// const result=document.getElementById("result")
-
-// const output = () => {
-//     html2canvas(document.getElementById("middle")).then(canvas => {
-//         const url = canvas.toDataURL("image/png")
-//         img.current.src = url
-//         a.current.href = url
-//     })
-// }
-
-console.log(html2canvas)
 html2canvas(document.getElementById("input")).then(canvas=>{
     const url = canvas.toDataURL("image/png")
     console.log(url)
 })
-
-console.log("666")
